0fec80650b663709b9eacba3f87dfb84bbddaf24
ngoossen-booth <ngoossen@chicagobooth.edu>
2020-10-07 21:08:44 +0000

    modified:   loops_fizz_buzz.rb 	modified:   loops_letter_count.rb fizzbuzz and letter count done 100%

--- loops_fizz_buzz.rb
+++ loops_fizz_buzz.rb
@@ -4,3 +4,17 @@
 # for multiples of three print "Fizz" instead of the number
 #   for multiples of five print "Buzz" instead of the number
 #   for numbers which are multiples of both three and five print "FizzBuzz"
+
+number = 1
+while number <= 100
+  if number % 3 == 0 and number % 5 == 0
+    p "FizzBuzz"
+  elsif number % 3 == 0
+    p "Fizz"
+  elsif number % 5 == 0
+    p "Buzz"
+  else 
+    p number
+  end
+  number = number + 1
+end
--- loops_letter_count.rb
+++ loops_letter_count.rb
@@ -14,3 +14,13 @@
 # "apple is 5 letters long!"
 
 p "Enter a word:"
+
+word = gets.chomp.downcase
+
+len = word.length
+i = 1
+while i <= len
+  p i
+  i=i+1
+end
+p "" + word +" is " + len.to_s + " letters long!"
